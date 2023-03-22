29c166e82593250cb6cf11752e1490963770c4d3
Dheerajsingh002 <dheerajsingh8357@gmail.com>
2023-03-23 00:40:20 +0530

    initial commit

--- index.js
+++ index.js
@@ -5,7 +5,11 @@
 // });
 
 
-var a=10;
-var b='10'+a; //not converted
-console.log(b);
-console.log(typeof b);
+// var a=10;
+// var b='10'+a; //not converted
+// console.log(b);
+// console.log(typeof b);
+var str="hello world";
+var"hello";
+var a=str.includes(sub);
+console.log(a);
